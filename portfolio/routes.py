fe70048096be259dec940a5795ed52f0b0336d4b
Corey <hamhams86@gmail.com>
2022-04-23 19:05:43 -0700

    initial

---
+++ portfolio/routes.py
@@ -0,0 +1,23 @@
+from flask import render_template, request
+from portfolio import app 
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+@app.route('/data-science')
+def data_science():
+    return render_template('data_science.html', title="Data Science")
+
+@app.route('/nlp')
+def nlp():
+    return render_template('nlp.html', title="NLP")
+
+@app.route('/kaggle')
+def kaggle():
+    return render_template('kaggle.html', title='Kaggle')
+
+@app.route('/unity')
+def unity():
+    return render_template('unity.html', title='Unity')
